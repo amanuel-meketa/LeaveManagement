12f42eb9ae4306ce1c9a7867316abd6e1833d5a1
Amanuel <Amanuel.Eshete@melkamsoft.com>
2023-10-20 22:06:55 +0300

    Added create command for LeaveRequests

---
+++ LeaveManagement/LeaveManagement.Application/Features/LeaveRequests/Handler/Commands/CreateLeaveRequestHandler.cs
@@ -0,0 +1,28 @@
+﻿using AutoMapper;
+using LeaveManagement.Application.Contracts.Persistence;
+using LeaveManagement.Application.Features.LeaveRequests.Request.Commands;
+using LeaveManagement.Domain;
+using MediatR;
+
+namespace LeaveManagement.Application.Features.LeaveRequests.Handler.Commands
+{
+    public class CreateLeaveRequestHandler : IRequestHandler<CreateLeaveRequest, Guid>
+    {
+        private readonly ILeaveRequestRepository _leaveRequestRepository;
+        private readonly IMapper _Mapper;
+
+        public CreateLeaveRequestHandler(ILeaveRequestRepository leaveRequestRepository, IMapper mapper)
+        {
+            _leaveRequestRepository = leaveRequestRepository;
+            _Mapper = mapper;
+        }
+
+        public async Task<Guid> Handle(CreateLeaveRequest request, CancellationToken cancellationToken)
+        {
+            var leaveRequest = _Mapper.Map<LeaveRequest>(request);
+            leaveRequest = await _leaveRequestRepository.Add(leaveRequest);
+
+            return leaveRequest.Id;
+        }
+    }
+}
---
+++ LeaveManagement/LeaveManagement.Application/Features/LeaveRequests/Request/Commands/CreateLeaveRequest.cs
@@ -0,0 +1,9 @@
+﻿using MediatR;
+
+namespace LeaveManagement.Application.Features.LeaveRequests.Request.Commands
+{
+    public class CreateLeaveRequest : IRequest<Guid>
+    {
+        public Guid Id { get; set; }
+    }
+}
