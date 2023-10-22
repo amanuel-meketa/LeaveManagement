4fd0becbf007bdedaeb53a2e851f44193082544c
Amanuel <Amanuel.Eshete@melkamsoft.com>
2023-10-22 11:11:23 +0300

    Implmented leave type request Delete command

---
+++ LeaveManagement/LeaveManagement.Application/Features/LeaveRequests/Handler/Commands/DeleteLeaveRequestHandler.cs
@@ -0,0 +1,25 @@
+﻿using AutoMapper;
+using LeaveManagement.Application.Contracts.Persistence;
+using LeaveManagement.Application.Features.LeaveRequests.Request.Commands;
+using MediatR;
+namespace LeaveManagement.Application.Features.LeaveRequests.Handler.Commands
+{
+    public class DeleteLeaveRequestHandler : IRequestHandler<DeleteLeaveRequest>
+    {
+        private readonly ILeaveRequestRepository _leaveRequestRepository;
+        private readonly IMapper _mapper;
+
+        public DeleteLeaveRequestHandler(ILeaveRequestRepository leaveRequestRepository, IMapper mapper)
+        {
+            _leaveRequestRepository = leaveRequestRepository;
+            _mapper = mapper;
+        }
+        public async Task<Unit> Handle(DeleteLeaveRequest request, CancellationToken cancellationToken)
+        {
+            var leaveRequest = await _leaveRequestRepository.Get(request.Id);
+            await _leaveRequestRepository.Delete(leaveRequest);
+
+            return Unit.Value;
+        }
+    }
+}
---
+++ LeaveManagement/LeaveManagement.Application/Features/LeaveRequests/Request/Commands/DeleteLeaveRequest.cs
@@ -0,0 +1,9 @@
+﻿using MediatR;
+
+namespace LeaveManagement.Application.Features.LeaveRequests.Request.Commands
+{
+    public class DeleteLeaveRequest : IRequest
+    {
+        public Guid Id { get; set; }
+    }
+}
