409da1802f2e8f81a738efc011dfd8a948034ee5
Amanuel <Amanuel.Eshete@melkamsoft.com>
2023-10-22 11:04:56 +0300

    Implmented leave type Delete command

---
+++ LeaveManagement/LeaveManagement.Application/Features/LeaveTypes/Handler/Commands/DeleteleaveTypeHandler.cs
@@ -0,0 +1,26 @@
+﻿using AutoMapper;
+using LeaveManagement.Application.Contracts.Persistence;
+using LeaveManagement.Application.Features.LeaveTypes.Request.Commands;
+using MediatR;
+
+namespace LeaveManagement.Application.Features.LeaveTypes.Handler.Commands
+{
+    public class DeleteleaveTypeHandler : IRequestHandler<DeleteleaveType>
+    {
+        private readonly ILeaveTypeRepository _leaveTypeRepository;
+        private readonly IMapper _mapper;
+
+        public DeleteleaveTypeHandler(ILeaveTypeRepository leaveTypeRepository, IMapper mapper)
+        {
+            _leaveTypeRepository = leaveTypeRepository;
+            _mapper = mapper;
+        }
+        public async Task<Unit> Handle(DeleteleaveType request, CancellationToken cancellationToken)
+        {
+            var leaveType = await _leaveTypeRepository.Get(request.Id);
+            await _leaveTypeRepository.Delete(leaveType);
+
+            return Unit.Value;
+        }
+    }
+}
---
+++ LeaveManagement/LeaveManagement.Application/Features/LeaveTypes/Request/Commands/DeleteleaveType.cs
@@ -0,0 +1,9 @@
+﻿using MediatR;
+
+namespace LeaveManagement.Application.Features.LeaveTypes.Request.Commands
+{
+    public class DeleteleaveType : IRequest
+    {
+        public Guid Id { get; set; }
+    }
+}
