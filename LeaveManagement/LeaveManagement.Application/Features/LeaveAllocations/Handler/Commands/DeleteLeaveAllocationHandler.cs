8ae1f3f7df40c9ec518d099b06a5ac641a69eb3e
Amanuel <Amanuel.Eshete@melkamsoft.com>
2023-10-22 11:16:04 +0300

    Implmented leave type allocation Delete command

---
+++ LeaveManagement/LeaveManagement.Application/Features/LeaveAllocations/Handler/Commands/DeleteLeaveAllocationHandler.cs
@@ -0,0 +1,25 @@
+﻿using AutoMapper;
+using LeaveManagement.Application.Contracts.Persistence;
+using LeaveManagement.Application.Features.LeaveAllocations.Request.Commands;
+using MediatR;
+
+namespace LeaveManagement.Application.Features.LeaveAllocations.Handler.Commands
+{
+    public class DeleteLeaveAllocationHandler : IRequestHandler<DeleteLeaveAllocation>
+    {
+        private readonly ILeaveAllocationRepository _leaveAllocationRepository;
+        private readonly IMapper _mapper;
+
+        public DeleteLeaveAllocationHandler(ILeaveAllocationRepository leaveAllocationRepository, IMapper mapper)
+        {
+            _leaveAllocationRepository = leaveAllocationRepository;
+            _mapper = mapper;
+        }
+        public async Task<Unit> Handle(DeleteLeaveAllocation request, CancellationToken cancellationToken)
+        {
+            var leaveAllocation = await _leaveAllocationRepository.Get(request.Id);
+            await _leaveAllocationRepository.Delete(leaveAllocation);
+            return Unit.Value;
+        }
+    }
+}
---
+++ LeaveManagement/LeaveManagement.Application/Features/LeaveAllocations/Request/Commands/DeleteLeaveAllocation.cs
@@ -0,0 +1,9 @@
+﻿using MediatR;
+
+namespace LeaveManagement.Application.Features.LeaveAllocations.Request.Commands
+{
+    public class DeleteLeaveAllocation : IRequest
+    {
+        public Guid Id { get; set; }
+    }
+}
