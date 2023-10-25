493dd650327cfcf6a3faa91abccd66f6c00eda7d
Amanuel <Amanuel.Eshete@melkamsoft.com>
2023-10-25 21:02:11 +0300

    added  EF sql and configered DbContext - override SaveChangesAsync for auditlog

---
+++ LeaveManagement/Persistence/Data/LeaveManagmentDbContext.cs
@@ -0,0 +1,34 @@
+﻿using LeaveManagement.Domain;
+using LeaveManagement.Domain.Common;
+using Microsoft.EntityFrameworkCore;
+
+namespace LeaveManagement.Persistence.Data
+{
+    public class LeaveManagmentDbContext : DbContext
+    {
+        public LeaveManagmentDbContext(DbContextOptions<LeaveManagmentDbContext> options) : base(options)
+        {    
+        }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfigurationsFromAssembly(typeof(LeaveManagmentDbContext).Assembly);
+        }
+
+        public override Task<int> SaveChangesAsync(CancellationToken cancellationToken = default)
+        {
+            foreach (var entry in ChangeTracker.Entries<BaseEntity>())
+            { 
+               entry.Entity.LastModifiedDate = DateTime.UtcNow;
+
+                if(entry.State == EntityState.Added)
+                    entry.Entity.DateCreated = DateTime.UtcNow;
+            }
+
+            return base.SaveChangesAsync(cancellationToken);
+        }
+
+        DbSet<LeaveType> leaveTypes { get; set; }
+        DbSet<LeaveRequest> leaveRequests { get; set; }
+        DbSet<LeaveAllocation> leaveAllocations { get; set; }
+    }
+}
