14b05703bcd5ae6b3ab380517d973a99d94bae49
Amanuel <Amanuel.Eshete@melkamsoft.com>
2023-10-25 22:41:20 +0300

    Added leave allocation repository implmentation

---
+++ LeaveManagement/Persistence/Repositorys/LeaveAllocationRepository.cs
@@ -0,0 +1,34 @@
+﻿using LeaveManagement.Application.Contracts.Persistence;
+using LeaveManagement.Domain;
+using LeaveManagement.Persistence.Data;
+using Microsoft.EntityFrameworkCore;
+
+namespace LeaveManagement.Persistence.Repositorys
+{
+    public class LeaveAllocationRepository : GenericRepository<LeaveAllocation>, ILeaveAllocationRepository
+    {
+        private readonly LeaveManagmentDbContext _dbContext;
+
+        public LeaveAllocationRepository(LeaveManagmentDbContext dbContext) : base(dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public async Task<List<LeaveAllocation>> GetLeaveAllocationDetail()
+        {
+            var leaveAllocations = await _dbContext.leaveAllocations
+                 .Include(l => l.LeaveType)
+                 .ToListAsync();
+            return leaveAllocations;
+        }
+
+        public async Task<LeaveAllocation> GetLeaveAllocationDetail(Guid id)
+        {
+            var leaveAllocations = await _dbContext.leaveAllocations
+                .Include(l => l.LeaveType)
+                .FirstOrDefaultAsync(i => i.Id == id);
+
+            return leaveAllocations;
+        }
+    }
+}
