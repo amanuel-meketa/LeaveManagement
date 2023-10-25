ce91e8d53b8e0f6bcb0abec62abfaa66ebd9b8cf
Amanuel <Amanuel.Eshete@melkamsoft.com>
2023-10-25 23:16:10 +0300

    Added Email sender service

---
+++ LeaveManagement/LeaveManagement.Application/Contracts/Infrastructure/IEmailService.cs
@@ -0,0 +1,9 @@
+﻿using LeaveManagement.Application.Models;
+
+namespace LeaveManagement.Application.Contracts.Infrastructure
+{
+    public interface IEmailService
+    {
+        Task<bool> SendEmail(Email email);
+    }
+}
---
+++ LeaveManagement/LeaveManagement.Application/Models/Email.cs
@@ -0,0 +1,10 @@
+﻿
+namespace LeaveManagement.Application.Models
+{
+    public class Email
+    {
+        public string? To { get; set; }
+        public string? Subject { get; set; }
+        public string? Body { get; set; }
+    }
+}
